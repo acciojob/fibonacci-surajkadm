67f2be26a4b2712e691b9e7915ad48affb4ea799
surajkadm <68494225+surajkadm@users.noreply.github.com>
2023-04-17 14:03:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function fibonacci(num) {
 	var current=1;
 		var prev=0;
 
-		for (let i = 0; i < num; i++) {
+		for (let i = 0; i < num-1; i++) {
 			var newFib=current+prev;
 			prev=current;
 			current=newFib;
