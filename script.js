7189fa73ea46bd09d90712cf56563e09dc19acef
surajkadm <68494225+surajkadm@users.noreply.github.com>
2023-04-17 14:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
-// your code here
-}
+	var current=1;
+		var prev=0;
+
+		for (let i = 0; i < num; i++) {
+			var newFib=current+prev;
+			prev=current;
+			current=newFib;
+		}
+		return current
+	}
 
 module.exports = fibonacci;
